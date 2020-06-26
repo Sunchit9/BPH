cf0c7fe17db57ce7c934560c6705f12eddb84544
Sunchit9 <sunchitbansal9@gmail.com>
2020-06-26 22:39:17 +0530

    front end

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,7 +19,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+app.use('/forgotpassword', function(req,res,next){
+  console.log("ForgotPassword Requested");
+  res.render("forgotpassword");
+});
+app.use('/login', function(req,res,next){
+  console.log("Login Page Requested");
+  res.render("login");
+});
+
+app.use('/signup', function(req,res,next){
+  console.log("Signup Page Requested");
+  res.render("signup");
+});
+app.use('/', function(req,res,next){
+  console.log("Home Page Requested");
+  res.render("home");
+});
+
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
