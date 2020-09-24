e1f0dc63fd4756b579858607667460ab7a63530c
Sunchit9 <sunchitbansal9@gmail.com>
2020-09-24 12:32:11 +0530

    update 2.0

--- routes/users.js
+++ routes/users.js
@@ -101,12 +101,11 @@ get( function(req, res, next) {
   else if(req.body.delete == 'Delete My Account')
   {
     User.findOneAndRemove({_id: req.params.id}, (err, response) => { 
-      Comments.remove({_id: { $in: req.body.eventsAttended }}, (err, res) => {
-         ...
-      })
-  })
-  }
+      
  } )
+}
+});
+
 
 router.get('/profile/:id', function(req, res, next) {
   if(req.user.provider!='google')
@@ -200,147 +199,6 @@ router.get('/logout', (req, res) => {
 })
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* GET users listing. */
-/*router.get('/', cors.corsWithOptions,authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
-  User.find({})
-  .then((users) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(users);
-  }, (err) => next(err))
-  .catch((err) => next(err));
-});
-
-router.post('/signup',cors.corsWithOptions, (req, res, next) => {
-  User.register(new User({username: req.body.username}), 
-    req.body.password, (err, user) => {
-    if(err) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({err: err});
-    }
-    else {
-      if (req.body.firstname)
-        user.firstname = req.body.firstname;
-      if (req.body.lastname)
-        user.lastname = req.body.lastname;
-      user.save((err, user) => {
-        if (err) {
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({err: err});
-          return ;
-        }
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({success: true, status: 'Registration Successful!'});
-        });
-      });
-    }
-  });
-});
-
-router.post('/login',cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
-  var token = authenticate.getToken({_id: req.user._id});
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, token: token, status: 'You are successfully logged in!'});
-});
-
-
-router.get('/logout', (req, res) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  }
-  else {
-    var err = new Error('You are not logged in!');
-    err.status = 403;
-    next(err);
-  }
-});
-
-router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
-  if (req.user) {
-    var token = authenticate.getToken({_id: req.user._id});
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.json({success: true, token: token, status: 'You are successfully logged in!'});
-  }
-});
-
-module.exports = router;
-*/
-
-
-
-
-
 
 
 
